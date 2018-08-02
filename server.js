1afa30ca08561345b70a29f871cb53cac7ee5788
avidrutham <divyamathur89@gmail.com>
2018-08-02 15:41:31 +0530

    first commit of timestamp microservice

--- server.js
+++ server.js
@@ -5,6 +5,9 @@
 var express = require('express');
 var app = express();
 
+// temporary port variable for local testing
+var port = process.env.PORT || 3000;
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -27,6 +30,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
